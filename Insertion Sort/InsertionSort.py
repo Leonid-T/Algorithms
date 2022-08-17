e0ccd1f76cea1da795fe3df6e9641fa601fcffb4
Leonid <titov281@yandex.ru>
2022-08-17 17:23:14 +0500

    Add reverse option

--- Insertion Sort/InsertionSort.py
+++ Insertion Sort/InsertionSort.py
@@ -1,15 +1,19 @@
-def insertion_sort(A):
+def insertion_sort(A, reverse=False):
+    if reverse:
+        compare = lambda x, y: x < y
+    else:
+        compare = lambda x, y: x > y
     for j in range(1, len(A)):
         key = A[j]
         i = j - 1
-        while i >= 0 and A[i] > key:
+        while i >= 0 and compare(A[i], key):
             A[i+1] = A[i]
             i = i - 1
         A[i+1] = key
     return A
 
 def main():
-    A = [5, 2, 4, 6, 1, 3]
+    A = [31, 41, 59, 26, 41, 58]
     insertion_sort(A)
     print(A)
 
