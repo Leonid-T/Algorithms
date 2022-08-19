7a21b368803653b3fa4b96d4d7d51df2d5be2159
Leonid <titov281@yandex.ru>
2022-08-19 14:33:29 +0500

    Update sum of two

--- Tasks/is_digit_is_sum_of_two_on_array.py
+++ Tasks/is_digit_is_sum_of_two_on_array.py
@@ -1,45 +1,15 @@
 def is_sum_of_two(x, A):
-    A0 = A.copy()
-    i = 0
-    while i < len(A0):
-        if A0[i] > x:
-            A0.pop(i)
-        else:
-            i += 1
-    if len(A0) == 0:
-        return False
-
-    middle = x // 2
-    A1 = []
-    A2 = []
-    counter = 0
-    for val in A0:
-        if middle < val:
-            A2.append(val)
-        elif val < middle:
-            A1.append(val)
-        elif middle*2 == x:
-            counter += 1
-            if counter == 2:
-                return True
-        else:
-            A1.append(val)    
-    if len(A1) == 0 or len(A2) == 0:
-        return False
-
-    for i in range(len(A1)):
-        A1[i] = x - A1[i]
     d = {}
-    for val in A1:
-        d[val] = True
-    for val in A2:
-        if d.get(val, False):
+    for i in range(len(A)):
+        d[A[i]] = i
+    for i in range(len(A)):
+        if d.get(x - A[i]) and d.get(x - A[i]) != i:
             return True
     return False
 
 def main():
-    A = [2, 4, 8, 4, 45, 23, 5, 7, 11, 10]
-    x = 8
+    A = [3, 4, 8, 4, 45, 23, 5, 7, 11, 10]
+    x = 17
     print(is_sum_of_two(x, A))
 
 if __name__ == '__main__':
