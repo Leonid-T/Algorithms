79c2e59153d3543d91eeafc64defbd54ddc9b59f
Leonid <titov281@yandex.ru>
2022-08-31 14:00:50 +0500

    Add reverse in heap sort, add priority queue

--- Heap sort/heap_sort.py
+++ Heap sort/heap_sort.py
@@ -1,37 +1,38 @@
-def parent(i):
-    return i // 2
-
 def left(i):
-    return 2*i
+    return 2*(i + 1) - 1
 
 def right(i):
-    return 2*i + 1
+    return 2*(i + 1)
 
-def max_heapify(A, i, heap_size):
+def heapify(A, i, heap_size, compare):
     l = left(i)
     r = right(i)
-    if l <= heap_size-1 and A[l] > A[i]:
+    if l <= heap_size-1 and compare(A[l], A[i]):
         largest = l
     else:
         largest = i
-    if r <= heap_size-1 and A[r] > A[largest]:
+    if r <= heap_size-1 and compare(A[r], A[largest]):
         largest = r
     if largest != i:
         A[i], A[largest] = A[largest], A[i]
-        max_heapify(A, largest, heap_size)
+        heapify(A, largest, heap_size, compare)
 
-def build_max_heap(A):
+def build_heap(A, compare):
     heap_size = len(A)
     for i in range(len(A)//2-1, -1, -1):
-        max_heapify(A, i, heap_size)
+        heapify(A, i, heap_size, compare)
 
-def heap_sort(A):
-    build_max_heap(A)
+def heap_sort(A, reverse=False):
+    if reverse:
+        compare = lambda x, y: x < y
+    else:
+        compare = lambda x, y: x > y
+    build_heap(A, compare)
     heap_size = len(A)
     for i in range(-1, -len(A)-1, -1):
         A[i], A[0] = A[0], A[i]
         heap_size -= 1
-        max_heapify(A, 0, heap_size)
+        heapify(A, 0, heap_size, compare)
 
 def main():
     A = [17, 27, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
